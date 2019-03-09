247aaed9ff3289187190b27f04ee1d90be84c388
angelacchung <44277537+angelacchung@users.noreply.github.com>
2019-03-09 18:03:47 -0500

    hm

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/angela', function () {
-    return view('angela');
+Route::get('/login', function () {
+    return view('login');
 });
 Route::get('/2', function () {
     return view('2');
