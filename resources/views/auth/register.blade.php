bac463f74d3e356a3200c80adcf5629b8a8d8a85
Alicia <alicia@canuckcollective.com>
2019-03-25 12:43:39 -0400

    Updated register.blade.php
    Removed form validation for form 2 and 3 & added placeholder values for all 3 forms & cleaned up comments

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,18 +8,9 @@
 
 
 <section class="testimonials_area">
-    <!-- <div class="form-group row">
-        <div class="col offset">
-            <h2 class="text-green text-center text-bold mt-5" id="register-heading">Create an account</h2>
-            <div class="d-none text-center" id="register-pagination">
-                <span class="dots"></span>
-                <span class="dots inactive"></span>
-            </div>
-        </div>
-    </div> -->
     <div class="testi_slider owl-carousel">
         <div>
-            <div class="testi_item">
+            <div class="testi_item"> <!-- Create An Account -->
                 <div class="container mt-4">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
@@ -41,7 +32,7 @@
                             <label for="lastName" class="col-md-4 col-form-label text-md-right">{{ __('Last Name') }}</label>
 
                             <div class="col-md-4 mb-3">
-                                <input id="lastName" type="text" class="shadow form-control{{ $errors->has('lastName') ? ' is-invalid' : '' }}" name="lastName" value="{{ old('lastName') }}" required>
+                                <input placeholder="Enter last name" id="lastName" type="text" class="shadow form-control{{ $errors->has('lastName') ? ' is-invalid' : '' }}" name="lastName" value="{{ old('lastName') }}" required>
 
                                 @if ($errors->has('lastName'))
                                     <span class="invalid-feedback" role="alert">
@@ -55,7 +46,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
 
                             <div class="col-md-4  mb-3">
-                                <input id="email" type="text" class="shadow form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                                <input placeholder="Enter email address" id="email" type="text" class="shadow form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
@@ -69,7 +60,7 @@
                             <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Phone') }}</label>
 
                             <div class="col-md-4  mb-3">
-                                <input id="phone" type="text" class="shadow form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" name="phone" value="{{ old('phone') }}" required>
+                                <input placeholder="Enter phone number" id="phone" type="text" class="shadow form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" name="phone" value="{{ old('phone') }}" required>
 
                                 @if ($errors->has('phone'))
                                     <span class="invalid-feedback" role="alert">
@@ -83,7 +74,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-4  mb-3">
-                                <input id="password" type="password" class="shadow form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                <input placeholder="Enter password" id="password" type="password" class="shadow form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback" role="alert">
@@ -97,7 +88,7 @@
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-4 mb-3">
-                                <input id="password-confirm" type="password" class="shadow form-control" name="password_confirmation" required>
+                                <input placeholder="Re-enter password" id="password-confirm" type="password" class="shadow form-control" name="password_confirmation" required>
                             </div>
                         </div>
 
@@ -125,10 +116,8 @@
         </div>
 
 
-
-
         <div>
-            <div class="testi_item">
+            <div class="testi_item"> <!-- Driver Info -->
                 <div class="container mt-4">
                     <div class="form-group row">
                         <div class="col">
@@ -145,14 +134,7 @@
                             <label for="dob" class="col-md-4 col-form-label text-md-right">{{ __('Date of birth ') }}</label>
 
                             <div class="col-md-4 mb-3">
-                                <input id="dob" type="date" class="shadow form-control{{ $errors->has('dob') ? ' is-invalid' : '' }}" name="dob" required>
-                                <!-- <input id="dob" type="password" class="shadow form-control{{ $errors->has('dob') ? ' is-invalid' : '' }}" name="dob" required> -->
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                                <input id="dob" type="date" class="shadow form-control" name="dob" required>
                             </div>
                         </div>
 
@@ -160,15 +142,15 @@
                             <label for="licenceNumber" class="col-md-4 col-form-label text-md-right">{{ __('Driver licence number') }}</label>
 
                             <div class="col-md-4 mb-3">
-                                <input id="licenceNumber" type="text" class="shadow form-control" name="licenceNumber" required>
+                                <input placeholder="Enter drivers licence number" id="licenceNumber" type="text" class="shadow form-control" name="licenceNumber" required>
                             </div>
                         </div>
 
                         <div class="form-group row">
                             <label for="province" class="col-md-4 col-form-label text-md-right">{{ __('Province') }}</label>
                             <div class="col-md-4 mb-3">
-                                <select id="province" class="shadow form-control" name="province" required>
-                                    <option value="">---</option>
+                                <select placeholder="Choose a province"id="province" class="shadow form-control" name="province" required>
+                                    <option value="" disabled selected>Select your province</option>
                                     <option value="Ontario">Ontario</option>
                                     <option value="Manitoba">Manitoba</option>
                                     <option value="Alberta">Alberta</option>
@@ -181,13 +163,7 @@
                                     <option value="New Brunswick">New Brunswick</option>
                                 </select>
                             </div>
-                            <!-- <label for="province" class="col-md-4 col-form-label text-md-right">{{ __('Province') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="province" type="number" class="shadow form-control" name="province" required>
-                            </div> -->
                         </div>
-                        
 
                         <div class="form-group row">
                             <label for="expiryDate" class="col-md-4 col-form-label text-md-right">{{ __('Date of expiration') }}</label>
@@ -211,9 +187,8 @@
         </div>
 
 
-
         <div>
-            <div class="testi_item">
+            <div class="testi_item"> <!-- Car Info -->
                 <div class="container mt-4">
                     <div class="form-group row">
                         <div class="col offset">
@@ -232,7 +207,7 @@
                             <div class="col-md-4 mb-3">
                                 
                             <select id="year" class="shadow form-control{{ $errors->has('year') ? ' is-invalid' : '' }}" name="year" required>
-                                <option value="">---</option>
+                                <option value="" disabled selected>Select vehicle year</option>
                                 <option value="2019">2019</option>
                                 <option value="2018">2018</option>
                                 <option value="2017">2017</option>
@@ -244,13 +219,6 @@
                                 <option value="2011">2011</option>
                                 <option value="2010">2010</option>
                             </select>
-                                <!-- <input id="year" type="text" class="shadow form-control{{ $errors->has('year') ? ' is-invalid' : '' }}" name="year" value="{{ old('year') }}" required> -->
-
-                                @if ($errors->has('year'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('year') }}</strong>
-                                    </span>
-                                @endif
                             </div>
                         </div>
 
@@ -258,28 +226,14 @@
                             <label for="make" class="col-md-4 col-form-label text-md-right">{{ __('Make') }}</label>
 
                             <div class="col-md-4 mb-3 ">
-                                <input id="make" type="text" class="shadow form-control{{ $errors->has('make') ? ' is-invalid' : '' }}" name="make" value="{{ old('make') }}" required>
-
-                                @if ($errors->has('make'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('make') }}</strong>
-                                    </span>
-                                @endif
+                                <input placeholder="Enter make (Ex: Honda)" id="make" type="text" class="shadow form-control" name="make" value="{{ old('make') }}" required>
                             </div>
                         </div>
 
                         <div class="form-group row">
                             <label for="model" class="col-md-4 col-form-label text-md-right">{{ __('Model') }}</label>
-                            <!-- like 'civic''prius' look up on google. make is the brand.model is type of car -->
                             <div class="col-md-4 mb-3">
-                                <input id="model" type="date" class="shadow form-control{{ $errors->has('model') ? ' is-invalid' : '' }}" name="model" required>
-                                <!-- <input id="dob" type="password" class="shadow form-control{{ $errors->has('model') ? ' is-invalid' : '' }}" name="model" required> -->
-
-                                @if ($errors->has('model'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('model') }}</strong>
-                                    </span>
-                                @endif
+                                <input placeholder="Enter model (Ex: Civic)" id="model" type="text" class="shadow form-control name="model" required>
                             </div>
                         </div>
 
@@ -287,50 +241,21 @@
                             <label for="licencePlate" class="col-md-4 col-form-label text-md-right">{{ __('Licence plate') }}</label>
 
                             <div class="col-md-4 mb-3">
-                                <input id="licencePlate" type="text" class="shadow form-control" name="licencePlate" required>
+                                <input placeholder="Enter licence plate"id="licencePlate" type="text" class="shadow form-control" name="licencePlate" required>
                             </div>
                         </div>
 
                         <div class="form-group row">
                             <label for="kilometers" class="col-md-4 col-form-label text-md-right">{{ __('Kilometers') }}</label>
                             <div class="col-md-4 mb-3">
-                                <select id="kilometers" class="shadow form-control" name="kilometers" required>
-                                    <!-- make a text field -->
-                                    <option value="">---</option>
-                                    <option value="Ontario">Ontario</option>
-                                    <option value="Manitoba">Manitoba</option>
-                                    <option value="Alberta">Alberta</option>
-                                    <option value="British Columbia">British Columbia</option>
-                                    <option value="Newfoundland">Newfoundland</option>
-                                    <option value="Saskatchewan">Saskatchewan</option>
-                                    <option value="Nova Scotia">Nova Scotia</option>
-                                    <option value="Prince Edward Island">Prince Edward Island</option>
-                                    <option value="Quebec">Quebec</option>
-                                    <option value="New Brunswick">New Brunswick</option>
-                                </select>
+                                <input placeholder="Enter odometer value" id="kilometers" type="text" class="shadow form-control" name="kilometers" required>
                             </div>
-                            <!-- <label for="province" class="col-md-4 col-form-label text-md-right">{{ __('Province') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="province" type="number" class="shadow form-control" name="province" required>
-                            </div> -->
                         </div>
-                        
 
                         <div class="form-group row">
                             <label for="color" class="col-md-4 col-form-label text-md-right">{{ __('Color') }}</label>
-                            <!-- make color as a text field -->
                             <div class="col-md-4 mb-3">
-                                <select id="color" class="shadow form-control" name="color" required>
-                                    <option value="">---</option>
-                                    <option value="Red">Red</option>
-                                    <option value="Green">Green</option>
-                                    <option value="Blue">Blue</option>
-                                    <option value="Black">Black</option>
-                                    <option value="Grey">Grey</option>
-                                    <option value="White">White</option>
-                                    <option value="Yellow">Yellow</option>
-                                </select>
+                                <input placeholder="Enter vehicle colour" id="color" type="text" class="shadow form-control" name="color" required>
                             </div>
                         </div>
 
