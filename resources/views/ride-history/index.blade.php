91c9469c5c2c44cf83e5bb660d347ff64d8d6fe0
Angela Chung <angelajjung@hotmail.ca>
2019-03-29 12:35:34 -0400

    edit

--- resources/views/ride-history/index.blade.php
+++ resources/views/ride-history/index.blade.php
@@ -10,7 +10,7 @@
 
     <div class="form-group row m-0">
         <div class="btn-group btn-group-toggle col" data-toggle="buttons">
-            <label class="btn btn-primary active p-4">
+            <label class="btn btn-secondary active p-4">
                 <input type="radio" name="options" id="past" autocomplete="off"checked> Past
             </label>
            
@@ -20,17 +20,32 @@
         </div> 
     </div>
 <!-- next section -->
-<div class="form-group row m-0">
-        <div class="btn-group btn-group-toggle col" data-toggle="buttons">
-            <label class="btn btn-primary active p-4">
-                <input type="radio" name="options" id="past" autocomplete="off"checked> Past
-            </label>
-           
-            <label class="btn btn-primary p-4">
-                <input type="radio" name="options" id="upcoming" autocomplete="off"> Upcoming
-            </label>
-        </div> 
+<div class="form-group row m-0 p-2 shadow-sm border-green-left mb-3">
+        
+        <div class="row">
+            <div class="col-4">
+                <img src="img/angela-1.jpg">
+            </div>
 
+             <div class="col-8">
+                 <p>David Smithe</p>
+            </div>     
+        
+        </div> 
+</div>
+
+<div class="form-group row m-0 p-2 shadow-sm border-blue-left mb-3">
+        
+        <div class="row">
+            <div class="col-4">
+                <img src="img/angela-1.jpg">
+            </div>
+
+             <div class="col-8">
+                 <p>David Smithe</p>
+            </div>     
+        
+        </div> 
 </div>
 
 
