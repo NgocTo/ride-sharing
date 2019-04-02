9083ff61796c31df7ba74ac759ad6bad6fbc01b8
WrenChen <42975345+WrenChen@users.noreply.github.com>
2019-04-02 14:07:00 -0400

    Update index.blade.php

--- resources/views/rides/index.blade.php
+++ resources/views/rides/index.blade.php
@@ -10,11 +10,11 @@
     <div class="form-group row m-0">
         <div class="btn-group btn-group-toggle col" data-toggle="buttons">
             <label class="btn btn-white text-primary active p-4">
-                <input type="radio" name="options" id="past" autocomplete="off"checked> Past
+                <input type="radio" name="options" id="past" autocomplete="off"> Past
             </label>
            
             <label class="btn btn-primary p-4">
-                <input type="radio" name="options" id="upcoming" autocomplete="off"> Upcoming
+                <input type="radio" name="options" id="upcoming" autocomplete="off"checked> Upcoming
             </label>
         </div> 
     </div>
@@ -36,24 +36,6 @@
         
 </div>
 
-<div class="form-group row m-0 p-4 shadow-sm border-blue-left mb-3">
-        
-    <div class="col-4">
-        <div class="img-container"><img src="{{ asset('img/woman1.jpg') }}"></div>
-        <p>8.6</p>
-
-    </div>
-
-    <div class="col-8">
-            <p class="name">Julia Doe</p>
-            <p>Pickup:Sheridan College Trafalgar campus</p>
-            <p>Drop off:Square One Go bus terminal</p>
-            <p>Date:April 5 2019</p>
-            <p>Pickup time:1:15pm</p>
-    </div>     
-      
-</div>
-
 <div class="form-group row m-0 p-4 shadow-sm border-green-left mb-3">
         
     <div class="col-4">
@@ -63,11 +45,9 @@
     </div>
 
     <div class="col-8">
-            <p class="name">Allison Chung</p>
-            <p>Pickup:Sheridan College Trafalgar campus</p>
-            <p>Drop off:Square One Go bus terminal</p>
-            <p>Date:April 5 2019</p>
-            <p>Pickup time:1:15pm</p>
+    @foreach($currentRides as $c)
+        <p>{{ $c->startPos }}</p>
+    @endforeach
     </div>     
       
 </div>
