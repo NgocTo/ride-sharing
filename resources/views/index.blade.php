9f2b5d0e326f499830e2b07f69a12b1a96778382
Angela Chung <angelajjung@hotmail.ca>
2019-04-08 15:32:36 -0400

    list of drivers css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,9 +8,9 @@
 @section('script')
 <script src="{{ asset('js/script.js') }}"></script>
 <script src="{{ asset('js/map.js') }}"></script>
-<script async defer
+<!-- <script async defer
 src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAQWLvcO1cPisBkY_Bo3w2YxbRk6pm9pVo&callback=initMap">
-</script>
+</script> -->
 <script>
 
 // toggle rider and driver
@@ -22,7 +22,98 @@ $( "#toggleBtn" ).click(function() {
 @endsection
 <div class="flash-message alert alert-success alert-block text-center fixed-top" role="alert"></div>
 
+
 <div>
+    <div class="direction" class="p-0">
+
+        <h2 class="bg-green text-white p-3 text-center m-0">Where are you going?</h2>
+            <form>
+            @csrf
+                <div class="form-group mx-5 mt-4">
+                    <label for="pickUp">Pickup</label>
+                    <input type="text" class="form-control" id="pickUp" name="pickUp" placeholder="Your pickup location">
+                    <div class="prediction-container"><div class="predictions"></div></div>
+                </div>
+
+                <div class="form-group mx-5 mt-4 ">
+                    <label for="dropOff">Destination</label>
+                    <input type="text" class="form-control" id="dropOff" name="dropOff" placeholder="Your destination">
+                    <div class="prediction-container"><div class="predictions"></div></div>
+                 </div>
+
+                @if (Auth::check() && ($user->ifDriver === 1) && Session::has('driverMode'))
+                <div class="form-group mx-5 mt-4 ">
+                    <label for="time">When are you going?</label>
+                    <input type="datetime-local" class="form-control" id="time" name="time">
+                </div>
+                <div class="form-group mx-5 mb-4">
+                    <button type="submit" id="submitTrip" class="text-bold btn btn-primary text-center btn-block">
+                        {{ __('Submit trip') }}
+                    </button>
+                </div>
+                @else
+                <div class="form-group mx-5 mb-4">
+                    <button type="submit" id="checkRide" class="text-bold btn btn-primary text-center btn-block">
+                        {{ __('See rides') }}
+                    </button>
+                </div>
+                @endif
+            </form>
+  <!-- display available rides -->
+    <div class="row m-0 p-0 mb-3">
+
+        <div class="col-4">
+            <div class="img-container"><img src="{{ asset('img/man1.jpg') }}"></div>
+            
+        </div>
+
+        <div class="col-5">
+                <p class="name mb-0">David Smithe</p>
+                <p class="ratingSmall">8.6</p>
+                <i class="fas fa-star starSmall"></i>
+                <p><strong>Rules: </strong>Please do not have any cat hair on you, I am allergic.</p>
+        </div>    
+            <div class="col-3">
+                <p class="text-green greenPrice">$5.55</p>
+                <p>5 minutes away</p>
+            </div>
+    </div>
+              
+</div>
+
+
+<!-- <div>
+    <div class="direction" class="p-0">
+
+        <h2 class="bg-green text-white p-3 text-center m-0">Drivers</h2>
+          @csrf
+            <div class="form-group mt-4">
+                <div class="row m-0 p-0 mb-3">
+
+                    <div class="col-4">
+                        <div class="img-container"><img src="{{ asset('img/man1.jpg') }}"></div>
+                        
+                    </div>
+        
+                    <div class="col-5">
+                        <p class="name mb-0">David Smithe</p>
+                        <p class="rating">8.6</p>
+                        <i class="fas fa-star"></i>
+                        <p><strong>Rules: </strong>Please do not have any cat hair on you, I am allergic.</p>
+                    </div>    
+                    <div class="col-3">
+                    <p>5 minutes away</p>
+                    </div>
+                </div>
+    
+             </div>
+                
+    </div> -->
+
+
+
+
+<!-- <div>
     <div class="direction" class="p-0">
 
         <h2 class="bg-green text-white p-3 text-center m-0">Where are you going?</h2>
@@ -59,7 +150,7 @@ $( "#toggleBtn" ).click(function() {
                 @endif
             </form>
                 
-    </div>
+    </div> -->
     <div id="map" class="p-0 m-0"></div>
 </div>
 <div id="directionResponse"></div>
