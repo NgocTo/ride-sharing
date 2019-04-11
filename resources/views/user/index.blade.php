27fe81ea01a8b40571c648864b3d1805efceaa53
angelacchung <44277537+angelacchung@users.noreply.github.com>
2019-04-10 22:07:35 -0400

    css editing

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -22,7 +22,7 @@
 
 
 
-<div class="container mt-4 ">
+<div class="mt-4">
     <div class="form-group row col-md-4 offset-md-4">
         <div class="col-5">
             <div class="img-container"><img src="{{ asset('img/default-profile.jpg') }}" alt="Avatar"></div>
@@ -33,10 +33,11 @@
             <p>Phone: {{$user->phone}}</p>
                     <!-- <p>Rules: {{$user->keyword}}</p> -->
         </div>
-        
-        <div class="m-4 row form-group">
-            <div class="col-6">
-                    <h3 class="h4">Driver Information<a href="#"><i class="fas fa-edit ml-3"></i></a></h3> 
+    </div>    
+
+        <div id="driverCarCont" class="form-group row col-md-8 offset-md-3 mt-4">
+            <div class="col-6 mt-4">
+                    <h3 class="name">Driver Information<a href="#"><i class="fas fa-edit ml-3"></i></a></h3> 
                     @if (isset($user->driverInfo))
                         <p>Date of birth: {{$user->driverInfo->dob}}</p>
                         <p>My licence Number: {{$user->driverInfo->licenceNumber}}</p>
@@ -49,8 +50,8 @@
             </div>
         
         
-        <div class="col-6">
-                    <h3 class="h4">Car Information <a href="#"><i class="fas fa-edit ml-3"></i></a></h3> 
+            <div class="col-6 mt-4">
+                    <h3 class="name">Car Information <a href="#"><i class="fas fa-edit ml-3"></i></a></h3> 
                     @if (isset($user->vehicleInfo))
                         <p>Model: {{$user->vehicleInfo->model}}</p>
                         <p>Car year: {{$user->vehicleInfo->carYear}}</p>
@@ -61,53 +62,8 @@
                     @endif
             </div>        
         </div>
-    </div>
-</div>
-
-
-
-<!-- old one -->
-<div class="container p-0">
-<div class="form-group row m-0 p-4"> 
-        
-         <div class="col-5">
-            <div class="img-container"><img src="{{ asset('img/default-profile.jpg') }}" alt="Avatar"></div>
-        </div>
     
-        <div class="col-7">
-        
-            
-                <p class="name mb-2">{{$user->firstName}} {{$user->lastName}}<a href="#"><i class="fas fa-edit ml-3"></i></a></p>
-                <p>Email: {{$user->email}}</p>
-                <p>Phone: {{$user->phone}}</p>
-                <!-- <p>Rules: {{$user->keyword}}</p> -->
-        </div>
 </div>
 
-    <div class="m-4 row form-group">
-        <div class="col-6">
-                <h3 class="h4">Driver Information<a href="#"><i class="fas fa-edit ml-3"></i></a></h3> 
-                @if (isset($user->driverInfo))
-                    <p>Date of birth: {{$user->driverInfo->dob}}</p>
-                    <p>My licence Number: {{$user->driverInfo->licenceNumber}}</p>
-                    <p>Expiry Date: {{$user->driverInfo->expiryDate}}</p>
-                    <p>Province: {{$user->driverInfo->province}}</p>
-                    <p>My rating: {{$user->driverInfo->driverRating}}</p>
-                    <p>My rules: {{$user->driverInfo->driverRules}}</p>
-                @endif
-        </div>
 
-        <div class="col-6">
-                <h3 class="h4">Car Information <a href="#"><i class="fas fa-edit ml-3"></i></a></h3> 
-                @if (isset($user->vehicleInfo))
-                    <p>Model: {{$user->vehicleInfo->model}}</p>
-                    <p>Car year: {{$user->vehicleInfo->carYear}}</p>
-                    <p>Make: {{$user->vehicleInfo->make}}</p>
-                    <p>Kilometers: {{$user->vehicleInfo->kilometers}}</p>
-                    <p>Color: {{$user->vehicleInfo->color}}</p>
-                    <p>Licence Plate: {{$user->vehicleInfo->licencePlate}}</p>
-                @endif
-        </div>        
-    </div>
-            
-</div>
+
